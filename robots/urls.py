d4ccc3a8b5bf3777de6eae0e96ea450e28e6ea0a
Дорджи Опиев <opiev.dordzhi@yandex.ru>
2023-09-29 17:09:40 +0300

    Task 2

--- robots/urls.py
+++ robots/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import add_robot
+from .views import add_robot, download_robots_in_excel
 
 app_name = "robots"
 
 urlpatterns = [
     path("robots", add_robot),
+    path("get_robots_report", download_robots_in_excel),
 ]
--- robots/views.py
+++ robots/views.py
@@ -1,10 +1,15 @@
+from datetime import timedelta
 import json
+import openpyxl
 
-from django.http import JsonResponse
+from django.db.models import Count
+from django.http import HttpResponse, JsonResponse
+from django.utils import timezone
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 
 from .forms import RobotForm
+from .models import Robot
 
 @csrf_exempt
 @require_http_methods(["GET", "POST"])
@@ -30,3 +35,36 @@ def add_robot(request):
             return JsonResponse({
                 "message": "Ошибка валидации формы",
                 "errors": form.errors})
+
+def download_robots_in_excel(request):
+    end_date = timezone.now()
+    start_date = end_date - timedelta(days=7)
+
+    robots = Robot.objects.filter(
+        created__range=(start_date, end_date)
+    ).values(
+        'model', 'version'
+    ).annotate(robot_count=Count('id'))
+
+    unique_models = Robot.objects.filter(
+        created__range=(start_date, end_date)
+    ).values_list('model', flat=True).distinct()
+    workbook = openpyxl.Workbook()
+    for model in unique_models:
+
+        model_robots = robots.filter(model=model)
+        worksheet = workbook.create_sheet(title=model)
+        worksheet.append(['Модель', 'Версия', 'Количество за неделю'])
+        worksheet.column_dimensions['C'].width = 24
+
+        for robot in model_robots:
+            worksheet.append([robot['model'], robot['version'], robot['robot_count']])
+
+    default_sheet = workbook.get_sheet_by_name('Sheet')
+    workbook.remove(default_sheet)
+
+    response = HttpResponse(content_type='application/ms-excel')
+    response['Content-Disposition'] = 'attachment; filename=robots_report.xlsx'
+    workbook.save(response)
+
+    return response
